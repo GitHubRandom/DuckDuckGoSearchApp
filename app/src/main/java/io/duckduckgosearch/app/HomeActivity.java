70250aa3178dddff7afc9acfa41535d4f5da3f73
S Md <sinchi_midou@hotmail.com>
2018-09-07 13:25:22 +0100

    Added search activity and shared element animation to search field

--- app/src/main/java/io/duckduckgosearch/app/HomeActivity.java
+++ app/src/main/java/io/duckduckgosearch/app/HomeActivity.java
@@ -1,13 +1,35 @@
 package io.duckduckgosearch.app;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class HomeActivity extends AppCompatActivity {
+
+    EditText searchField;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        context = this;
+
+        searchField = findViewById(R.id.search_field);
+        searchField.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, SearchActivity.class);
+                ActivityOptionsCompat optionsCompat = ActivityOptionsCompat
+                        .makeSceneTransitionAnimation(HomeActivity.this,
+                                searchField, context.getResources().getString(R.string.search_bar_transition_name));
+                startActivity(intent, optionsCompat.toBundle());
+            }
+        });
     }
 }
