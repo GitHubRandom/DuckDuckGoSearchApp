b0a3d2157ae4aa8f84e1869c6ca03680ce2acedb
S Md <sinchimidou@gmail.com>
2019-06-27 01:37:12 +0100

    Subtle improvement on history fragment

--- app/src/main/java/io/duckduckgosearch/app/HistoryFragment.java
+++ app/src/main/java/io/duckduckgosearch/app/HistoryFragment.java
@@ -1,6 +1,8 @@
 package io.duckduckgosearch.app;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -12,6 +14,7 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import androidx.annotation.NonNull;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -22,38 +25,52 @@ public class HistoryFragment extends BottomSheetDialogFragment {
 
     static final String HISTORY_DB_NAME = "history_db";
 
+    private Context context;
     private HistoryAdapter adapter;
     private HistoryDatabase historyDatabase;
     private ArrayList<HistoryItem> historyList = new ArrayList<>();
+    private TextView fragmentTitle;
+    private RecyclerView historyListRv;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
 
     @Override
     public void setupDialog(Dialog dialog, int style) {
         View view = View.inflate(getContext(), R.layout.fragment_history, null);
 
-        historyDatabase = Room.databaseBuilder(getContext(), HistoryDatabase.class, HISTORY_DB_NAME).build();
+        historyDatabase = Room.databaseBuilder(context, HistoryDatabase.class, HISTORY_DB_NAME).build();
 
         CoordinatorLayout root = view.findViewById(R.id.search_history_root);
-        final TextView fragmentTitle = view.findViewById(R.id.history_fragment_title);
-        final RecyclerView historyListRv = view.findViewById(R.id.history_fragment_list);
+        fragmentTitle = view.findViewById(R.id.history_fragment_title);
+        historyListRv = view.findViewById(R.id.history_fragment_list);
         historyListRv.setHasFixedSize(true);
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                historyList = (ArrayList<HistoryItem>) historyDatabase.historyDao().getAllSearchHistory();
-                if (historyList.size() == 0) {
-                    fragmentTitle.setText(R.string.search_history_empty);
-                }
-                Collections.reverse(historyList);
-                adapter = new HistoryAdapter(getContext(), historyList);
-                getActivity().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        historyListRv.setAdapter(adapter);
+        if (PrefManager.isHistoryEnabled(context)) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    historyList = (ArrayList<HistoryItem>) historyDatabase.historyDao().getAllSearchHistory();
+                    if (historyList.size() == 0) {
+                        fragmentTitle.setText(R.string.search_history_empty);
                     }
-                });
-            }
-        }).start();
+                    Collections.reverse(historyList);
+                    adapter = new HistoryAdapter(getContext(), historyList);
+                    ((Activity) context).runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            historyListRv.setAdapter(adapter);
+                        }
+                    });
+                }
+            }).start();
+        } else {
+            fragmentTitle.setText(R.string.search_history_disabled);
+        }
+
 
         ImageView dragIcon = view.findViewById(R.id.history_fragment_drag_icon);
         RecyclerView.LayoutManager manager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false);
