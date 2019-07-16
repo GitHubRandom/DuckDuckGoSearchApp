6c6beb2fa65803c9a0d4ad0af9dbc8338df37ec5
S Md <sinchimidou@gmail.com>
2019-07-16 21:16:38 +0100

    Limited search suggestions to 5

--- app/src/main/java/io/duckduckgosearch/app/AutoCompleteAdapter.java
+++ app/src/main/java/io/duckduckgosearch/app/AutoCompleteAdapter.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Filter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -15,6 +16,9 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * This is a custom adapter for search suggestions list
@@ -26,6 +30,8 @@ public class AutoCompleteAdapter extends ArrayAdapter<String> {
     private int resId;
     private AutoCompleteTextView searchBar;
     private OnItemClickListener clickListener;
+    private String[] filteredList;
+    private String[] list;
 
     public interface OnItemClickListener {
         void onItemClickListener(String searchTerm);
@@ -36,7 +42,83 @@ public class AutoCompleteAdapter extends ArrayAdapter<String> {
         super(context, resource, objects);
         this.context = context;
         resId = resource;
+        list = objects;
+        filteredList = Arrays.copyOf(list, 5, String[].class);
         this.searchBar = searchBar;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                FilterResults results = new FilterResults();
+
+                if (list == null || list.length == 0) {
+                    synchronized (this) {
+                        results.values = null;
+                        results.count = 0;
+                    }
+                } else {
+                    if (charSequence == null || charSequence.length() == 0) {
+                        synchronized (this) {
+                            results.values = Arrays.copyOf(list, 5, String[].class);
+                            results.count = 5;
+                        }
+                    } else {
+                        ArrayList<String> matchingTerms = new ArrayList<>();
+                        for (String item : list) {
+                            if (item.toLowerCase().startsWith(charSequence.toString().toLowerCase())) {
+                                matchingTerms.add(item);
+                            }
+                        }
+                        if (matchingTerms.size() < 5) {
+                            synchronized (this) {
+                                results.values = Arrays.copyOf(matchingTerms.toArray(), matchingTerms.size(), String[].class);
+                                results.count = matchingTerms.size();
+                            }
+                        } else {
+                            synchronized (this) {
+                                results.values = Arrays.copyOf(matchingTerms.toArray(), 5, String[].class);
+                                results.count = 5;
+                            }
+                        }
+                    }
+                }
+
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                if (filterResults == null) {
+                    filteredList = new String[]{};
+                    notifyDataSetInvalidated();
+                } else {
+                    filteredList = (String[]) filterResults.values;
+                    if (filterResults.count > 0) {
+                        notifyDataSetChanged();
+                    } else {
+                        notifyDataSetInvalidated();
+                    }
+                }
+                Log.d("Filter", Arrays.toString(filteredList));
+            }
+        };
+    }
+
+    @Override
+    public String getItem(int position) {
+        return filteredList[position];
+    }
+
+    @Override
+    public int getCount() {
+        if (filteredList != null) {
+            return filteredList.length;
+        } else {
+            return 0;
+        }
     }
 
     @NonNull
