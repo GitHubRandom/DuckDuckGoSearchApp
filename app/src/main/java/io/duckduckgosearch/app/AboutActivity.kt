04e7fdfac49ffb27c820780244c90ca81777841a
Mehdi ESSALEHI <messalehi2001@outlook.com>
2020-12-28 01:40:54 +0100

    Rewrote AboutActivity

--- app/src/main/java/io/duckduckgosearch/app/AboutActivity.kt
+++ app/src/main/java/io/duckduckgosearch/app/AboutActivity.kt
@@ -1,39 +1,35 @@
-package io.duckduckgosearch.app;
+package io.duckduckgosearch.app
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.cardview.widget.CardView;
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.cardview.widget.CardView
+import androidx.core.content.res.ResourcesCompat
 
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.widget.TextView;
-
-public class AboutActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
         if (PrefManager.isDarkTheme(this)) {
-            setTheme(R.style.AppTheme_Dark);
+            setTheme(R.style.AppTheme_Dark)
         }
-        setContentView(R.layout.activity_about);
-
-        String versionName = "";
+        setContentView(R.layout.activity_about)
+        var versionName: String? = ""
         try {
-            versionName = this.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName;
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
+            versionName = this.packageManager.getPackageInfo(this.packageName, 0).versionName
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
         }
-        ((TextView)findViewById(R.id.version_name_text)).setText(getResources().getString(R.string.settings_about_version, versionName));
-
+        findViewById<TextView>(R.id.version_name_text).text = resources.getString(R.string.settings_about_version, versionName)
         if (PrefManager.isDarkTheme(this)) {
-            TextView disclaimerMessage = findViewById(R.id.disclaimer_message);
-            disclaimerMessage.setTextColor(getResources().getColor(android.R.color.white));
-            TextView disclaimerTitle = findViewById(R.id.disclaimer_title);
-            disclaimerTitle.setTextColor(getResources().getColor(R.color.darkThemeColorAccent));
-            CardView disclaimerContainer = findViewById(R.id.disclaimer_container);
-            disclaimerContainer.setCardBackgroundColor(getResources().getColor(R.color.darkThemeColorSearchFieldBg));
+            val disclaimerMessage = findViewById<TextView>(R.id.disclaimer_message)
+            disclaimerMessage.setTextColor(ResourcesCompat.getColor(resources,android.R.color.white,null))
+            val disclaimerTitle = findViewById<TextView>(R.id.disclaimer_title)
+            disclaimerTitle.setTextColor(ResourcesCompat.getColor(resources,R.color.darkThemeColorAccent,null))
+            val disclaimerContainer = findViewById<CardView>(R.id.disclaimer_container)
+            disclaimerContainer.setCardBackgroundColor(ResourcesCompat.getColor(resources,R.color.darkThemeColorSearchFieldBg,null))
         }
     }
 }
