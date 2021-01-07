484fe05f9fac0d993f0a950d190ef48f76c7b6ad
Mehdi ESSALEHI <messalehi2001@outlook.com>
2021-01-07 14:07:01 +0100

    Implemented onConfigurationChanged(), transparent status bar and correct theme if app started from activity

--- app/src/main/java/io/duckduckgosearch/app/AboutActivity.kt
+++ app/src/main/java/io/duckduckgosearch/app/AboutActivity.kt
@@ -1,6 +1,7 @@
 package io.duckduckgosearch.app
 
 import android.content.pm.PackageManager
+import android.content.res.Configuration
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,13 +11,21 @@ class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+        AppCompatDelegate.setDefaultNightMode(PrefManager.getThemeInt(this))
+        this.transparentStatusBar()
         setContentView(R.layout.activity_about)
-        var versionName: String? = ""
+        var versionName = ""
         try {
             versionName = this.packageManager.getPackageInfo(this.packageName, 0).versionName
         } catch (e: PackageManager.NameNotFoundException) {
             e.printStackTrace()
         }
         findViewById<TextView>(R.id.version_name_text).text = resources.getString(R.string.settings_about_version, versionName)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        finish()
+        startActivity(intent)
     }
 }
